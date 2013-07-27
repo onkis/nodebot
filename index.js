acc0725426b6cf0fae332b0371e1b628277516f6
Mike Ball <mike.ball3@gmail.com>
2013-07-27 16:24:04 -0400

    crap

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
-var five = require("johnny-five"),
-    // or "./lib/johnny-five" when running from the source
-    board = new five.Board();
-
-board.on("ready", function() {
-
-  // Create an Led on pin 13 and strobe it on/off
-  // Optionally set the speed; defaults to 100ms
-  (new five.Led(1)).on();
-
-});
-
 // var five = require("johnny-five"),
-//     board, motor, led;
-
-// board = new five.Board();
+//     // or "./lib/johnny-five" when running from the source
+//     board = new five.Board();
 
 // board.on("ready", function() {
-//   // Create a new `motor` hardware instance.
-//   motor = new five.Motor({
-//     pin: 1
-//   });
 
-//   // Inject the `motor` hardware into
-//   // the Repl instance's context;
-//   // allows direct command line access
-//   board.repl.inject({
-//     motor: motor
-//   });
+//   // Create an Led on pin 13 and strobe it on/off
+//   // Optionally set the speed; defaults to 100ms
+//   (new five.Led(1)).on();
 
-//   // Motor Event API
-
-//   // "start" events fire when the motor is started.
-//   motor.on("start", function( err, timestamp ) {
-//     console.log( "start", timestamp );
-
-//     // Demonstrate motor stop in 2 seconds
-//     // board.wait( 2000, function() {
-//     //   motor.stop();
-//     // });
-//   });
-
-//   // "stop" events fire when the motor is started.
-//   motor.on("stop", function( err, timestamp ) {
-//     console.log( "stop", timestamp );
-//   });
-
-//   // Motor API
-
-//   // start()
-//   // Start the motor. `isOn` property set to |true|
-//   motor.start();
-
-//   // stop()
-//   // Stop the motor. `isOn` property set to |false|
 // });
+
+var five = require("johnny-five"),
+    board, motor, led;
+
+board = new five.Board();
+
+board.on("ready", function() {
+  // Create a new `motor` hardware instance.
+  motor = new five.Motor({
+    pin: 12
+  });
+
+  // Inject the `motor` hardware into
+  // the Repl instance's context;
+  // allows direct command line access
+  board.repl.inject({
+    motor: motor
+  });
+
+  // Motor Event API
+
+  // "start" events fire when the motor is started.
+  motor.on("start", function( err, timestamp ) {
+    console.log( "start", timestamp );
+
+    // Demonstrate motor stop in 2 seconds
+    // board.wait( 2000, function() {
+    //   motor.stop();
+    // });
+  });
+
+  // "stop" events fire when the motor is started.
+  motor.on("stop", function( err, timestamp ) {
+    console.log( "stop", timestamp );
+  });
+
+  // Motor API
+
+  // start()
+  // Start the motor. `isOn` property set to |true|
+  motor.start();
+
+  // stop()
+  // Stop the motor. `isOn` property set to |false|
+});
 
 
 
